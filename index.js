d25a40ba3f6fe27403a62e066e110315d8065271
ElenaKars <elena_baldina@yahoo.com>
2024-12-12 14:02:34 +0100

    js update

--- index.js
+++ index.js
@@ -3,7 +3,16 @@ const cardEmployeeEl = document.getElementById("card-employee");
 
 const getCard = (event) => {
   event.preventDefault();
-  console.log("event happened");
+  const nameEmpl = formEl.name.value.trim();
+  const surnameEmpl = formEl.surname.value.trim();
+  const ageEmpl = formEl.age.value.trim();
+  const jobEmpl = formEl.job.value.trim();
+
+  if (nameEmpl !== "" && surnameEmpl !== "" && ageEmpl !== "") {
+    cardEmployeeEl.style.opacity = "100";
+  } else {
+    alert("Fill the required fields (*)");
+  }
 };
 
 formEl.addEventListener("submit", getCard);
