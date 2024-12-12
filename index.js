9cfe00d07864e049f1aea6a9d5144d0b7616d808
s-saratov <saratovs@yandex.ru>
2024-12-12 14:54:02 +0100

    comments create

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Создаём переменные - ссылки на необходимые элементы страницы
+
 const formEl = document.getElementById("form-employee");
 const cardEmployeeEl = document.getElementById("card-employee");
 const nameLabel = document.getElementById("name_employee");
@@ -5,22 +7,32 @@ const surnameLabel = document.getElementById("surname-employee");
 const ageLabel = document.getElementById("age-employee");
 const jobLabel = document.getElementById("job-employee");
 
+// Создаём функцию, отвечающую за передачу значений из полей ввода в текст карточки и её демонстрацию
+
 const getCard = (event) => {
-  event.preventDefault();
+  event.preventDefault(); // предоставращаем стандартную отработку события
+
+  // Создаём переменные, в которые передаём необходимые значения
   const nameEmpl = formEl.name.value.trim();
   const surnameEmpl = formEl.surname.value.trim();
   const ageEmpl = formEl.age.value.trim();
   const jobEmpl = formEl.job.value.trim();
+
+  // Условный оператор, предотвращающий передачу значений из незаполненных необходимых полей
   if (nameEmpl !== "" && surnameEmpl !== "" && ageEmpl !== "") {
+    
+    // Передаём значения в текст карточки
     nameLabel.textContent = nameEmpl;
     surnameLabel.textContent = surnameEmpl;
     ageLabel.textContent = ageEmpl;
     jobLabel.textContent = jobEmpl;
 
+    // Показывем заполненную карточку
     cardEmployeeEl.style.visibility = "visible";
   } else {
-    alert("Fill the required fields (*)");
+    alert("Fill all required fields (*)");
   }
 };
 
+// Запускаем ожидание события нажатия на кнопку
 formEl.addEventListener("submit", getCard);
